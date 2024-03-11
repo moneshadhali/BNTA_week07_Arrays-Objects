114718cfa3f6fd007197e1eac631344676a7631c
Monesha <moneshadhali@gmail.com>
2024-03-11 14:09:20 +0000

    Question 5 completed in arrays.js

--- code/arrays.js
+++ code/arrays.js
@@ -47,8 +47,8 @@ console.log("The third station is " + thirdStation);
  * Q5.
  * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
  */
-
-// console.log(stations);
+stations[1] = "Berwick-upon-Tweed";
+console.log(stations);
 
 
 
