b87e65e41e0152b07aa37a06f42d03f76315c11f
Monesha <moneshadhali@gmail.com>
2024-03-11 14:42:31 +0000

    Question 4 completed in objects.js

--- code/objects.js
+++ code/objects.js
@@ -43,15 +43,15 @@ console.log("The cat is a " + catBreed);
  * Q3. It's the tortoise's birthday! Add one to her current age.
  */
 tortoise.age = tortoise.age + 1;
-console.log(tortoise);
+// console.log(tortoise);
 
 /**
  * Q4. How many different colours is Poppy?
  */
 
-dogColourCount = null;
+dogColourCount = dog.colours.length;
 
-// console.log("The dog has " + dogColourCount + " different colours");
+console.log("The dog has " + dogColourCount + " different colours");
 
 /**
  * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
