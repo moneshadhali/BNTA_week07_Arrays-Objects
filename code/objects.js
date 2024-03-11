4284aeaba4e57f4a1225221138f56a6bbd71025a
Monesha <moneshadhali@gmail.com>
2024-03-11 14:51:16 +0000

    Question 6 completed in objects.js

--- code/objects.js
+++ code/objects.js
@@ -60,16 +60,16 @@ parrot = {
   name:"Lucky",
   age: 12,
   breed: "African Grey",
-  colours: []
+  colours: ["grey"]
 }
 
-console.log(parrot);
+// console.log(parrot);
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
  */
-
-// console.log(tortoise);
+tortoise.favouriteFood = "dandelion";
+console.log(tortoise);
 
 /**
  * Q7. What's the combined age of all the pets?
