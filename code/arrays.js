6f8a00de247032b2681b972550523719d65d6bbd
Monesha <moneshadhali@gmail.com>
2024-03-11 14:04:07 +0000

    Question 2 completed

--- code/arrays.js
+++ code/arrays.js
@@ -17,8 +17,8 @@ console.log(stations);
  * Q2.
  * It doesn't go as far as Dundee either, so remove it from the start of the array.
  */
-
-// console.log(stations);
+stations.shift();
+console.log(stations);
 
 
 
